7effc3df96126b1fa724a53079a6f0d64a7795e5
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-07-10 21:57:18 +0530

    commit

--- Python_jspyder/Conditional_Statement/elif condition.py
+++ Python_jspyder/Conditional_Statement/elif condition.py
@@ -4,7 +4,7 @@ class Condition2:
     if a>20:
         print("Nuber is greater than 20")
 
-    elif a==20:
+    elif a==20:   # This line will execute when if block condition will not satisfied.
         print("Number is equal to 20")
     
 
---
+++ Python_jspyder/Conditional_Statement/else.py
@@ -0,0 +1,12 @@
+class ElseCondition:
+    
+    a= int(input("Enter the number only but not equal to 30 \n"))
+    
+    if a<30:
+        print("print less than 30")
+    
+    
+    else:      # here if block condtion will not pass then else block will execute
+        print("print greater than 30")
+        
+        
