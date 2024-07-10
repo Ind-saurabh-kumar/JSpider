4128c490ab32325abe9bbd485381a97051b5ade8
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-07-10 21:53:48 +0530

    elif

---
+++ Python_jspyder/Conditional_Statement/elif condition.py
@@ -0,0 +1,14 @@
+class Condition2:
+    a=int(input("Enter Number only \n"))
+
+    if a>20:
+        print("Nuber is greater than 20")
+
+    elif a==20:
+        print("Number is equal to 20")
+    
+
+    else:
+        print("Number is Smaller than 20")
+
+    
