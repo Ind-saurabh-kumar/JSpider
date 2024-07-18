5343a385a88ae3b2ae3b183938c5b9cad6d003bd
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-07-18 09:19:04 +0530

    finallyblock

---
+++ Python_jspyder/Error and Exception/FinallyBlock.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 09:14:49 2024
+
+@author: saura
+"""
+
+
+
+class TestFinally:
+    
+    def displayFinallyClause(self, a,b):
+        
+        try:
+            c=a/b
+            print(c)
+            
+        except ZeroDivisionError:
+            print("Denomnator should not be equal to 0")
+            
+        finally:
+            print("**********Does not matter what ever executed***")
+
+
+
+t=TestFinally()
+t.displayFinallyClause(10, 0)
