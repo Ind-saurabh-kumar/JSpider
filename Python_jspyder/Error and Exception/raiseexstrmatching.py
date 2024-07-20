5a22397eee418b042c1a996847b536d3ac4633d0
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-07-20 09:21:17 +0530

    python

---
+++ Python_jspyder/Error and Exception/raiseexstrmatching.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 09:00:12 2024
+
+@author: saura
+"""
+
+
+
+class StringMissMatching(BaseException):
+    
+    
+    def __init__(self):
+        
+        message="String is not Matching..."
+    
+        super().__init__(message)
+
+
+class StrMatch:
+    
+    def strm(self,a,b):
+        if a!=b:
+            raise StringMissMatching()
+        else:
+            print("Both String are Same......")
+    
+    
+s=StrMatch()
+s.strm("Apple", "Appl")
+
+  
+                            
+        
+                
