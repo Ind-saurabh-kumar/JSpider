a95d46bd9ed392eb849efad28271ebc6741f90bd
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-08-06 18:25:11 +0530

    commit

---
+++ Python_jspyder/Assignment/Logical/LeapYear.py
@@ -0,0 +1,16 @@
+
+
+class LeapYear:
+
+    def leapY(self):
+        year=int(input("Enter the Year: \n"))
+
+        if (year%100 != 0 or year%400 == 0) and year % 4==0:
+            print(f"{year} is Leap year.")
+
+        else:
+            print(f"{year} is not Leap year.")
+
+
+ly=LeapYear()
+ly.leapY()
