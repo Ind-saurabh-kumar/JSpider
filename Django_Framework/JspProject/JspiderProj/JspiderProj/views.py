0ff14eb267699d2fa30abf7886501c98bced7c52
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-09-11 00:50:11 +0530

    commit

---
+++ Django_Framework/JspProject/JspiderProj/JspiderProj/views.py
@@ -0,0 +1,31 @@
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Product
+
+def home(request):
+    
+    data=Product()
+    
+    data.name="Traditional Telephone"
+    data.authorname=""
+    data.newprice=50000
+    data.oldprice=50000
+    
+    data.change=""
+    
+    data.discount=round(((data.oldprice-data.newprice)/data.oldprice)*100,2)
+    
+    if data.discount>=0:
+        data.change="Discount by"
+    else:
+        data.change="Increase by"
+        data.incr=round(((data.newprice-data.oldprice)/data.newprice)*100)
+        
+    
+   
+    
+    
+    return render(request, 'index.html', {'data':data})
