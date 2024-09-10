0b73f2e6177125495066a890bb34e8b1ab9085a1
ind-saurabh-kumar <saurabhkumar.conn@gmail.com>
2024-09-10 17:29:22 +0530

    commit

--- JavaScript/Array/array.js
+++ JavaScript/Array/array.js
@@ -6,4 +6,35 @@ let arr=new Array();
 let arr2=[1,3,4,5,6,7,8,9,10,11,12,13]
 
 
-// 
+// length
+
+console.log(arr.length);
+
+console.log(arr2.length);
+
+
+// push
+
+arr.push(30);
+console.log(arr);
+
+
+//pop
+
+let res=arr2.pop();
+console.log(res);
+console.log(arr2);
+
+
+//shift
+
+let sr=arr2.shift(1 );
+
+console.log("shift opertation", sr);
+console.log(sr);
+
+
+// Unshift
+
+
+console.log(arr2);
